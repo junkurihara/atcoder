6c7973f0955b8b12bbef4e503275679fa77f1253
Jun Kurihara <kurihara@ieee.org>
2021-11-12 01:23:30 +0900

    rust dfs snippet

--- rust/snippet/src/search.rs
+++ rust/snippet/src/search.rs
@@ -1,41 +1,47 @@
 use proconio::input;
 // use proconio::marker::Chars;
+use std::cell::RefCell;
 use std::io::{stdout, BufWriter, Write};
+use std::rc::Rc;
 
 pub fn main() {
   let out = stdout();
   let mut out = BufWriter::new(out.lock());
   input! {n: u32, m: u32} // n: edge num, m: path num
-  let mut graph: Vec<Node> = Vec::new();
+  let mut graph: Vec<Rc<RefCell<Node>>> = Vec::new();
   for i in 1..n + 1 {
-    graph.push(Node::new(i));
+    graph.push(Rc::new(RefCell::new(Node::new(i))));
   }
   for _ in 0..m {
     input! {from: usize, to: usize}
-    let child = graph[to - 1].clone();
-    println!("{:?}", child);
-    graph[from - 1].add_child(&child);
+    let rc = graph.get(to - 1).unwrap().clone();
+    let root = graph.get_mut(from - 1).unwrap();
+    root.borrow_mut().add_child(rc);
   }
 
   input! {root_idx: u32, target_idx: u32}
   let mut visited: Vec<bool> = vec![false; n as usize];
 
-  search_dfs(&graph[root_idx as usize - 1], &mut visited, target_idx);
+  search_dfs(
+    graph.get(root_idx as usize - 1).unwrap(),
+    &mut visited,
+    target_idx,
+  );
 
-  println!("{:#?}", graph);
+  // println!("{:#?}", graph);
 }
 
-fn search_dfs(root: &Node, stamp: &mut Vec<bool>, target: u32) {
-  println!("Visited {}", root.idx);
-  if target == root.idx {
+fn search_dfs(root: &Rc<RefCell<Node>>, stamp: &mut Vec<bool>, target: u32) {
+  println!("Visited {}", root.borrow().idx);
+  if target == root.borrow().idx {
     println!("Found!");
     return;
   }
-  stamp[root.idx as usize - 1] = true;
+  stamp[root.borrow().idx as usize - 1] = true;
 
-  for n in root.children.iter() {
-    println!("{} next => {}", root.idx, n.idx);
-    if stamp[n.idx as usize - 1] == false {
+  for n in root.borrow().children.iter() {
+    // println!("{} next => {}", root.borrow().idx, n.borrow().idx);
+    if stamp[n.borrow().idx as usize - 1] == false {
       search_dfs(n, stamp, target);
     }
   }
@@ -44,19 +50,19 @@ fn search_dfs(root: &Node, stamp: &mut Vec<bool>, target: u32) {
 #[derive(Debug, Clone)]
 struct Node {
   idx: u32,
-  children: Vec<Node>,
+  children: Vec<Rc<RefCell<Node>>>,
 }
 
 impl Node {
   pub fn new(idx: u32) -> Self {
-    return Node {
+    Node {
       idx,
       children: Vec::new(),
-    };
+    }
   }
 
-  fn add_child(&mut self, child: &Node) {
-    self.children.push(child.clone());
+  fn add_child(&mut self, child: Rc<RefCell<Node>>) {
+    self.children.push(child);
   }
 }
 
