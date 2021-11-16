b139cbcc9dca5e33e507832b44d27be2b6811292
Jun Kurihara <kurihara@ieee.org>
2021-11-16 16:45:27 +0900

    update DP snippet

--- java/snippet/TripleStep.java
+++ java/snippet/TripleStep.java
@@ -8,6 +8,8 @@ public class TripleStep {
     int n = sc.nextInt();
     Stair s = new Stair(n);
     System.out.println(s.calc(n));
+    Stair s2 = new Stair(n);
+    System.out.println(s.calcLoop(n));
   }
 
   private static class Stair {
@@ -17,13 +19,34 @@ public class TripleStep {
       this.stairPattern = new long[n + 1];
     }
 
+    // bottom up
+    private long calcLoop(int length) {
+      for (int i = 0; i < length + 1; i++) {
+        if (i == 0) {
+          this.stairPattern[0] = 1;
+          continue;
+        }
+        if (i == 1) {
+          this.stairPattern[1] = 1;
+          continue;
+        }
+        if (i == 2) {
+          this.stairPattern[2] = 2;
+          continue;
+        }
+        this.stairPattern[i] = this.stairPattern[i - 1] + this.stairPattern[i - 2] + this.stairPattern[i - 3];
+      }
+      return this.stairPattern[length];
+    }
+
+    // top down recursive
     private long calc(int remain) {
       if (remain == 1)
         return 1; // (1)
       if (remain == 2)
         return 2; // (1, 1) or (2)
       if (remain == 3)
-        return 3; // (1, 1, 1) or (1, 2) or (2, 1) or (3)
+        return 4; // (1, 1, 1) or (1, 2) or (2, 1) or (3)
       if (this.stairPattern[remain] == 0) {
         this.stairPattern[remain] = this.calc(remain - 1) + this.calc(remain - 2) + this.calc(remain - 3);
       }
