578232c7e091a78eb5889f8c745fba600977a24d
Jun Kurihara <kurihara@ieee.org>
2021-11-24 00:04:33 +0900

    add abc181 a

--- java/abc181/A.java
+++ java/abc181/A.java
@@ -3,5 +3,8 @@ import java.util.*;
 public class A {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println((n % 2 == 1) ? "Black" : "White");
+
   }
 }
