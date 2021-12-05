bb2d52787c5ee2faa689d8591dbad50c9a414f87
Jun Kurihara <kurihara@ieee.org>
2021-12-05 17:37:25 +0900

    fix abc230 b

--- java/abc230/B.java
+++ java/abc230/B.java
@@ -4,33 +4,37 @@ public class B {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String[] s = sc.next().split("");
-    int startIdx = 0;
+
+    int startIdx = -1;
     for (int i = 0; i < Math.min(3, s.length); i++) {
       if (s[i].equals("o")) {
         startIdx = i;
         break;
-      } else if (!(s[i].equals("x"))) {
+      } else if (!s[i].equals("x")) {
         System.out.println("No");
         return;
       }
     }
-    if (startIdx == 0 && s[0] != "o") {
+    if (startIdx == -1) {
       if (s.length >= 3) {
         System.out.println("No");
         return;
+      } else {
+        System.out.println("Yes");
+        return;
       }
-      startIdx = 3 + s.length;
     }
 
     String[] sample = "oxx".split("");
 
     for (int i = startIdx; i < s.length; i++) {
-      if (!(s[i].equals(sample[(i - startIdx) % sample.length]))) {
+      if (!sample[(i - startIdx) % 3].equals(s[i])) {
         System.out.println("No");
         return;
       }
     }
     System.out.println("Yes");
+
   }
 
 }
